960fa9ae90b94d91be9bb494a8cb72fc6f90d2b8
David Skinner <skinner.david@gmail.com>
2018-07-13 08:56:05 -0500

    Completed config structs. Writing config stuff.

--- pkg/config/structs.go
+++ pkg/config/structs.go
@@ -19,6 +19,12 @@ type Owner struct {
 	email string
 }
 
+// Default Owner.
+func (o *Owner) Default() {
+	o.Id = 0
+	o.email = "email"
+}
+
 // Ports base config interface for server io.
 type Ports struct {
 	Http  uint16
@@ -26,10 +32,35 @@ type Ports struct {
 	Rpc   uint16
 }
 
+// Default Ports.
+func (p *Ports) Default() {
+	p.Http = 80
+	p.Https = 433
+	p.Rpc = 5000
+}
+
 // Server base config interface to find server.
 type Server struct {
 	Host string
 	Ports
+}
+
+// Default Server receives choice string for host.
+func (s *Server) Default(choice string) {
+	// Default Host names
+	switch choice {
+	case "game":
+		s.Host = "davsk.sytes.net"
+		break
+	case "acct":
+		s.Host = "universe.gameserve.com"
+		break
+	default:
+		s.Host = choice
+	}
+
+	// Default Ports
+	s.Ports.Default()
 }
 
 // Connect base config interface to connect.
@@ -41,11 +72,44 @@ type Connect struct {
 	Password string
 }
 
+// Default Connect receives choice of databases.
+func (c *Connect) Default(choice string) {
+	switch choice {
+	case "game":
+		c.Host = "localhost"
+		c.Port = 5432
+		c.Database = "universe"
+		c.User = "postgres"
+		c.Password = "password"
+		break
+	case "acct":
+		c.Host = "localhost"
+		c.Port = 5432
+		c.Database = "business"
+		c.User = "postgres"
+		c.Password = "password"
+		break
+	default:
+		c.Host = "localhost"
+		c.Port = 5432
+		c.Database = "postgres"
+		c.User = "postgres"
+		c.Password = "password"
+	}
+}
+
 // Client config interface for all clients.
 type Client struct {
 	Title string
 	Owner
 	Game Server
+}
+
+// Default Client receives title.
+func (c *Client) Default(title string) {
+	c.Title = title
+	c.Owner.Default()
+	c.Game.Default("game")
 }
 
 // GameServer config interface for LAN  server.
@@ -56,11 +120,26 @@ type GameServer struct {
 	Game Connect
 }
 
+// Default GameServer
+func (gs *GameServer) Default(title string) {
+	gs.Title = title
+	gs.Ports.Default()
+	gs.Acct.Default("acct")
+	gs.Game.Default("game")
+}
+
 // AcctServer config interface for WAN server.
 type AcctServer struct {
 	Title string
 	Ports
 	Acct Connect
+}
+
+// Default AcctServer receives title string.
+func (as *AcctServer) Default(title string) {
+	as.Title = title
+	as.Ports.Default()
+	as.Acct.Default("acct")
 }
 
 // TestServer config interface for test server.
@@ -70,4 +149,15 @@ type TestServer struct {
 	AcctServer Ports
 	Game       Connect
 	Acct       Connect
+}
+
+// Default TestServer receives title string.
+func (ts *TestServer) Default(title string) {
+	ts.Title = title
+	ts.GameServer.Default("game")
+	ts.AcctServer.Http = 8000
+	ts.AcctServer.Https = 8080
+	ts.AcctServer.Rpc = 5001
+	ts.Game.Default("game")
+	ts.Acct.Default("acct")
 }
