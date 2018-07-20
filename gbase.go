9cb07a35df6982a4f32e95cb687fa54f4536067f
David Skinner <skinner.david@gmail.com>
2018-07-20 05:31:48 -0500

    Completed gbase.go. Working on high level program entry.

---
+++ gbase.go
@@ -0,0 +1,21 @@
+// /////////////////////////////////////////////////////////////
+// 'gbase.go'                                                  /
+//                                                             /
+// Copyright (c) 2018 Davsk℠. All Rights Reserved.             /
+// Use of this source code is governed by an ISC License (ISC) /
+// that can be found in the LICENSE file.                      /
+//                                                             /
+// by David Skinner                                            /
+// on July 20, 2018                                            /
+// for Davsk℠ Universe 4.0 project gbase                       /
+//                                                             /
+// /////////////////////////////////////////////////////////////
+
+// package gbase implements game support modules
+// for Universe 4.0 game.
+//
+// Overview
+//
+// Modules may be run as discreet programs on different computers networked
+// together or may be combined to form a standalone game for one computer.
+package gbase
